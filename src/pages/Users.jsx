f5837d2376ebb64103699a63865b13d83e4f9b60
Jhun Carlo <ramosjhuncarlo@gmail.com>
2025-04-04 16:56:21 +0800

    Updates

--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -1,32 +1,7 @@
-import { useParams } from "react-router-dom";
-import { useEffect, useState } from "react";
-import axios from "axios";
-import { Link } from "react-router-dom";
+import React from "react";
 
-function Users() {
-  const { id } = useParams();
-  const [user, setUser] = useState({});
-
-  async function fetchUserData() {
-    const { data } = await axios.get(`https://jsonplaceholder.typicode.com/users/${id}`);
-
-    console.log(data);
-
-    setUser(data);
-  }
-
-  useEffect(() => {
-    fetchUserData();
-  }, []);
-
-  return (
-    <div>
-      <Link to="/">Back to home</Link>
-      <h1>ID: {user.id}</h1>
-      <h1>Name: {user.name}</h1>
-      <h1>Username: {user.username}</h1>
-    </div>
-  );
-}
+const Users = () => {
+  return <div>Users</div>;
+};
 
 export default Users;
